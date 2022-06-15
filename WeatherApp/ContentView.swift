d1de5d12fe83f24a30fbf2f0a48e6eeba5b7ce4f
Kush Shah <kush2shah@yahoo.com>
2022-06-15 17:23:08 -0400

    HStack with days of the week

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -16,22 +16,40 @@ struct ContentView: View {
                 .edgesIgnoringSafeArea(.all)
             VStack{
                 Text("Chapel Hill, NC")
-                    .font(.system(size:32,
+                    .font(.system(size: 26,
                                   weight: .medium,
                                   design: .default))
                     .foregroundColor(.white)
-                    .padding(50)
+                    .padding(30)
                 
                 VStack{
-                    Image(systemName: "thermometer.sun.fill")
+                    Image(systemName: "sun.max.fill")
                         .renderingMode(.original)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(width: 180, height: 180)
+                        .frame(width: 90, height: 90)
                     
                     Text("76°")
-                        .font(.system(size: 70, weight: .medium))
+                        .font(.system(size: 30, weight: .medium))
                         .foregroundColor(.white)
+                }
+                Spacer()
+                HStack{
+                    DailyView(dayOfWeek: "TUE",
+                              conditions: "snowflake",
+                              temperature: 2)
+                    DailyView(dayOfWeek: "WED",
+                              conditions: "tornado",
+                              temperature: 25)
+                    DailyView(dayOfWeek: "THU",
+                              conditions: "hurricane",
+                              temperature: 80)
+                    DailyView(dayOfWeek: "FRI",
+                              conditions: "sun.max.fill",
+                              temperature: 110)
+                    DailyView(dayOfWeek: "SAT",
+                              conditions: "sun.haze.fill",
+                              temperature: 82)
                 }
                 Spacer()
             }
@@ -42,5 +60,33 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DailyView: View {
+    
+    var dayOfWeek: String
+    var conditions: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack{
+            Text(dayOfWeek)
+                .font(.system(size: 12,
+                              weight: .light,
+                              design: .default))
+                .foregroundColor(.white)
+                .padding(5)
+            
+            Image(systemName: conditions)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 20, height: 20)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 10, weight: .medium))
+                .foregroundColor(.white)
+        }
     }
 }
